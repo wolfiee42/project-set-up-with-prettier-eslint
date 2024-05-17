5b974e363213427ccac3ee429d8475355bf5e079
wolfiee42 <saifalislam2022@gmail.com>
2024-05-17 16:07:06 +0600

    project set up with prettier and eslint

---
+++ server.js
@@ -0,0 +1,4 @@
+"use strict";
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import app from './app';
+import configaration from './configaration';
+import mongoose from 'mongoose';
+
+async function main() {
+  try {
+    await mongoose.connect(configaration.database_url as string);
+    app.listen(configaration.port, () => {
+      console.log(`Example app listening on port ${configaration.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
